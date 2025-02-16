62c633a15a2c6f1e49535d7ad30fb8c556919316
macosnik <macosnik@icloud.com>
2025-02-16 11:33:25 +0300

    -

--- TechnicalSite/drawing_symbols.py
+++ TechnicalSite/drawing_symbols.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Возвращаем HTML с текстом "TextFromImage"
-    return open('technical_website.html')
+    return open('html.html')
 
 # Запускаем приложение
 if __name__ == '__main__':
