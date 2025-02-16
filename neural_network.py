097ced65cd3afbeec2cf45afb76a784ce7a809b7
macosnik <macosnik@icloud.com>
2025-02-16 10:20:39 +0300

    -

---
+++ neural_network.py
@@ -0,0 +1,8 @@
+import numpy
+
+class CharacterRecognition:
+    def __init__(self, inputs, hidden_layers, outputs, learning_rate=0.01):
+        self.inputs = inputs
+        self.hidden_layers = hidden_layers
+        self.outputs = outputs
+        self.learning_rate = learning_rate
