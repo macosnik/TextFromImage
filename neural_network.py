30f84160cb8c03130c1ef7d7e538159db26659d4
macosnik <macosnik@icloud.com>
2025-02-16 23:16:18 +0300

    -

--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,6 @@
 import numpy
 
-class CharacterRecognition:
+class NeuralNetwork:
     def __init__(self, inputs, hidden_layers, outputs, learning_rate=0.01):
         self.inputs = inputs
         self.hidden_layers = hidden_layers
--- parser.py
+++ parser.py
@@ -4,8 +4,8 @@ import time
 start = time.time()
 
 image = load('')
-image = simplify(image)
-# image = compression(image, 120, 120)
+# image = simplify(image)
+# image = compression(image, 28, 28)
 # image = draw_line(image, 100, 0, 1000, 500, (255, 0, 0))
 save(image, 'output.bmp')
 
