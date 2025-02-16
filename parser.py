82a4fca62a0dea35135bd909b10c49b39b749187
macosnik <macosnik@icloud.com>
2025-02-16 11:35:47 +0300

    -

--- parser.py
+++ parser.py
@@ -3,9 +3,9 @@ import time
 
 start = time.time()
 
-image = load('TechnicalSite/icon.png')
+image = load('test.jpg')
 image = simplify(image)
-image = compression(image, 180, 180)
+# image = compression(image, 180, 180)
 # image = draw_line(image, 100, 0, 1000, 500, (255, 0, 0))
 save(image, 'output.bmp')
 
