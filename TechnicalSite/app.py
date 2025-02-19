824be212867ddad63d02d6b6c62352a84b7e498d
macosnik <macosnik@icloud.com>
2025-02-19 23:55:53 +0300

    -

--- TechnicalSite/app.py
+++ TechnicalSite/app.py
@@ -1,112 +1,44 @@
-# Flask и компоненты для веб-приложения
-from flask import Flask, render_template, request, jsonify
+import flask, os, base64, threading, image_utils
 
-# Работа с файловой системой
-import os
+app = flask.Flask(__name__)
 
-# Декодирование base64
-from base64 import b64decode
-
-# Наш модуль для обработки изображений
-import image_utils
-
-# Параллельный процесс
-from threading import Thread
-
-# Создание Flask приложения
-app = Flask(__name__)
-
-# Обработка информации
 def photo_processing(name_file):
-    # Открытие изображения
-    image = image_utils.load(name_file)
+    image = image_utils.Image(name_file)
+    image.compression(32, 32)
+    image.save(name_file)
 
-    # Сжатие размера изображения
-    image = image_utils.compression(image, 32, 32)
-
-    # Сохранение изображения в директорию
-    image_utils.save(image, f"{name_file[:-3]}bmp")
-
-    # Удаление старого файла
-    os.remove(name_file)
-
-# Маршрут для главной страницы
 @app.route('/')
 def home():
-    # Рендеринг html шаблона из папки templates
-    return render_template('page.html')
+    return flask.render_template('page.html')
 
 @app.route('/get-image-count')
 def get_image_count():
-    # Получение названия папки из javascript
-    folder = request.args.get('folder', 'tests')
+    target_folder = f"../DataCenter/{flask.request.args.get('folder', 'tests')}"
+    count = len(os.listdir(target_folder))
+    return flask.jsonify({'count': count})
 
-    # Получаем путь к выбранной папке
-    target_folder = os.path.join('../BaseOfData', folder)
-
-    try:
-        # Количество файлов в папке
-        count = len(os.listdir(target_folder))
-
-        # Отправляем количество файлов п папке
-        return jsonify({'count': count})
-
-    except Exception as e:
-        # Выводим ошибку
-        print(f"Error getting image count: {e}")
-
-        # Отправляем ошибку
-        return jsonify({'count': 0})
-
-
-# Маршрут для сохранения изображений
 @app.route('/save-image', methods=['POST'])
 def save_image():
     try:
-        # Получение json данных
-        data = request.json
+        data = flask.request.json
 
-        # Извлечение имени папки из запроса
-        folder = data.get('folder', 'tests')
+        target_folder = f'../DataCenter/{data.get('folder', 'tests')}'
+        num_files = len(os.listdir(target_folder))
+        name_file = f'{target_folder[target_folder.rfind("/") + 1:]}_{num_files + 1}.bmp'
+        file_path = f'{target_folder}/{name_file}'
 
-        # Создание полного пути к целевой папке
-        target_folder = os.path.join('../BaseOfData', folder)
-
-        # Создание папки (если не существует)
-        os.makedirs(target_folder, exist_ok=True)
-
-        # Подсчет количества файлов для генерации имени
-        num_picture = len(os.listdir(target_folder))
-
-        # Генерация имени файла
-        name_file = f'{target_folder[target_folder.rfind("/") + 1:]}_{num_picture + 1}.png'
-
-        # Полный путь для сохранения файла
-        file_path = os.path.join(target_folder, name_file)
-
-        # Сохранение изображения
         with open(file_path, 'wb') as f:
-            # Разделение данных изображения на части (тип и данные)
-            img_data = data['image'].split(',')[1]
+            img_data = str(data['image'])[str(data['image']).rfind(','):]
+            f.write(base64.b64decode(img_data))
 
-            # Запись в файл
-            f.write(b64decode(img_data))
-
-        # Полный путь к файлу
         all_path = f'{os.getcwd()[:os.getcwd().rfind('/')]}/{target_folder[3:]}/{name_file}'
+        threading.Thread(target=photo_processing, args=(all_path,)).start()
 
-        # Создаём процесс для обработки фотографии
-        Thread(target=photo_processing, args=(all_path,)).start()
-
-        # Возврат ответа
-        return jsonify({'success': True})
+        return flask.jsonify({'success': True})
 
     except Exception as e:
-        # Обработка ошибок: вывод в консоль и возврат сообщения
         print(f"Error: {str(e)}")
-
-        return jsonify({'success': False, 'error': str(e)})
+        return flask.jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    # Хост 0.0.0.0 позволяет доступ с любого IP
     app.run(host='0.0.0.0', debug=True, port=1324)
