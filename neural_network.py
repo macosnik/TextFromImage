2924188558a551d8754f5eda53fd30ba7faf0de7
macosnik <macosnik@icloud.com>
2025-02-22 23:26:09 +0300

    -

--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,13 @@
 import random, math
 
+def sigmoid(i):
+    return 1 / (2.718281828459045 ** (- i) + 1)
+
+def sigmoid_derivative(i):
+    return (1 - i) * i
+
+
+
 class NeuralNetwork:
     def __init__(self, inputs, hidden_1, hidden_2, outputs):
         self.inputs = inputs
@@ -13,7 +21,15 @@ class NeuralNetwork:
         self.bias_2 = []
         self.bias_3 = []
 
-
+        for _ in range(inputs):
+            for _ in range(hidden_1):
+                self.weights_1.append(round(random.uniform(-0.5, 0.5), 2))
+        for _ in range(hidden_1):
+            for _ in range(hidden_2):
+                self.weights_2.append(round(random.uniform(-0.5, 0.5), 2))
+        for _ in range(hidden_2):
+            for _ in range(outputs):
+                self.weights_3.append(round(random.uniform(-0.5, 0.5), 2))
 
         for _ in range(hidden_1):
             self.bias_1.append(0.0)
