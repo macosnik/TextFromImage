64bf5778af236b6ae626d0ff5cc2907a1fd00e34
macosnik <macosnik@icloud.com>
2025-02-22 21:49:15 +0300

    -

--- neural_network.py
+++ neural_network.py
@@ -1,10 +1,27 @@
-import numpy
+import random, math
 
 class NeuralNetwork:
-    def __init__(self, inputs, hidden_layers, outputs):
+    def __init__(self, inputs, hidden_1, hidden_2, outputs):
         self.inputs = inputs
-        self.hidden_layers = hidden_layers
+        self.hidden_1 = hidden_1
+        self.hidden_2 = hidden_2
         self.outputs = outputs
+        self.weights_1 = []
+        self.weights_2 = []
+        self.weights_3 = []
+        self.bias_1 = []
+        self.bias_2 = []
+        self.bias_3 = []
+
+
+
+        for _ in range(hidden_1):
+            self.bias_1.append(0.0)
+        for _ in range(hidden_2):
+            self.bias_2.append(0.0)
+        for _ in range(outputs):
+            self.bias_3.append(0.0)
+
 
 
 
---
+++ training.py
@@ -0,0 +1,4 @@
+from neural_network import NeuralNetwork
+
+network = NeuralNetwork(625, 16, 16, 10)
+
