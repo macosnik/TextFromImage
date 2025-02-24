1c853c37b7f13245331441e4c000fba76bc4440c
macosnik <macosnik@icloud.com>
2025-02-24 16:38:01 +0300

    -

--- training.py
+++ training.py
@@ -1,12 +1,17 @@
 from neural_network import TwoLayersNeuralNetwork
 from image_utils import Image
+import time
 
 image = Image('DataCenter/tests/image_1.bmp')
 arr = image.zero_to_one_list()
 
-network = TwoLayersNeuralNetwork(625, 128, 64, 10)
+start_time = time.time()
 
+network = TwoLayersNeuralNetwork(625, 128, 64, 10)
+# network.forward(arr)
 network.train(arr, [0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+
+print(time.time() - start_time)
 
 
 
