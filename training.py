627f75c0305e8994857d772603224860c25a25bb
macosnik <macosnik@icloud.com>
2025-02-24 19:36:04 +0300

    -

--- training.py
+++ training.py
@@ -2,34 +2,28 @@ from neural_network import TwoLayersNeuralNetwork
 from image_utils import Image
 import os
 
-image_0 = []
-image_1 = []
+symbols_lib = ["0", "1"]
+lib = []
 
-epochs = 200
-
-for num_image in range(epochs):
-    image = Image(f'DataCenter/0/image_{num_image + 1}.bmp')
-    arr = image.zero_to_one_list()
-    image_0.append(arr)
-
-for num_image in range(epochs):
-    image = Image(f'DataCenter/1/image_{num_image + 1}.bmp')
-    arr = image.zero_to_one_list()
-    image_1.append(arr)
+for symbol in symbols_lib:
+    for num_image in range(len(os.listdir(f'DataCenter/{symbol}'))):
+        image = Image(f'DataCenter/{symbol}/image_{num_image + 1}.bmp')
+        arr = image.zero_to_one_list()
+        lib.append((arr, symbol))
 
 network = TwoLayersNeuralNetwork(625, 256, 128, 2)
 
-for epoch in range(epochs):
+for epoch in range(len(symbols_lib)):
     network.train(image_0[epoch], [1, 0])
-    network.train(image_1[epoch], [0, 1])
 
-image = Image(f'DataCenter/tests/image_1.bmp')
-arr = image.zero_to_one_list()
-print(network.forward(arr))
-
-image = Image(f'DataCenter/tests/image_2.bmp')
-arr = image.zero_to_one_list()
-print(network.forward(arr))
+#
+# image = Image(f'DataCenter/tests/image_1.bmp')
+# arr = image.zero_to_one_list()
+# print(network.forward(arr))
+#
+# image = Image(f'DataCenter/tests/image_2.bmp')
+# arr = image.zero_to_one_list()
+# print(network.forward(arr))
 
 
 
